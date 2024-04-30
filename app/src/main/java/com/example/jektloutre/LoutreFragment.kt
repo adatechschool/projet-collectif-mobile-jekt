0270f9608250347c6e91f0b876519702a0fa47e0
julieATS <julie-bonnin97@hotmail.fr>
2024-04-30 16:49:33 +0200

    added val buttonReturn

--- app/src/main/java/com/example/jektloutre/LoutreFragment.kt
+++ app/src/main/java/com/example/jektloutre/LoutreFragment.kt
@@ -1,6 +1,8 @@
 package com.example.jektloutre
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,10 @@ class LoutreFragment : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val buttonReturn = findViewById<Button>(R.id.ReturnList)
+        buttonReturn.setOnClickListener { startActivity(Intent(applicationContext,MainActivity::class.java))
         }
     }
 }
